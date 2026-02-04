24ae1e54f2e9541a00feed98449635094d3b3150
BinxHere <85563794+BinxHere@users.noreply.github.com>
2026-02-04 10:11:35 +0000

    full rewrite, should make this reload issue I found better(?)

--- YTLogoSubscriptions.js
+++ YTLogoSubscriptions.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name  YT Logo Redirector
 // @namespace  https://violentmonkey.github.io/
-// @version  1.0.2
+// @version  1.1.0
 // @description  Make the YouTube logo in the top left redirect to the Subscriptions page instead of Home
 // @author  BinxHere
 // @match  https://*.youtube.com/*
@@ -13,19 +13,11 @@
 // @grant  none
 // ==/UserScript==
 
-const TARGET_URL = "https://www.youtube.com/feed/subscriptions"; // Could redirect to anything, but honestly I'd just redirect it to Subscriptions
-
-function patchLogo() {
-  const logo = document.querySelector('a#logo');
-  if (!logo) return;
-
-  logo.href = TARGET_URL;
-
-  logo.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = TARGET_URL;
-  }, true);
-}
-
-const observer = new MutationObserver(patchLogo);
-observer.observe(document.documentElement, { childList: true, subtree: true });
+const TARGET_URL = "/feed/subscriptions";
+document.addEventListener('click', (e) => {
+    const logoLink = e.target.closest('a#logo');
+    if (logoLink) {
+        e.preventDefault();
+        window.location.href = TARGET_URL;
+    }
+}, true);
