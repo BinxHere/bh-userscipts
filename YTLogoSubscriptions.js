ac7f4854d64b84f6246101b892c3b53bb96e4022
BinxHere <85563794+BinxHere@users.noreply.github.com>
2026-02-04 10:03:53 +0000

    add icon, remove useless stuff from header

--- YTLogoSubscriptions.js
+++ YTLogoSubscriptions.js
@@ -1,27 +1,16 @@
 // ==UserScript==
-// @name         YT Logo Redirector
-// @namespace    https://violentmonkey.github.io/
-// @version      1.0.1
+// @name  YT Logo Redirector
+// @namespace  https://violentmonkey.github.io/
+// @version  1.0.2
 // @description  Make the YouTube logo in the top left redirect to the Subscriptions page instead of Home
-// @author       BinxHere
-// @match       https://*.youtube.com/*
-// @match       http://*.youtube.com/*
-// @run-at      document-start
-// @license      GNU V2
+// @author  BinxHere
+// @match  https://*.youtube.com/*
+// @match  http://*.youtube.com/*
+// @run-at  document-start
+// @license  GNU V2
+// @icon  https://raw.githubusercontent.com/BinxHere/bh-userscipts/refs/heads/main/ytredirectorlogo.png
 // @updateURL	 https://raw.githubusercontent.com/BinxHere/bh-userscipts/refs/heads/main/YTLogoSubscriptions.js
-// @compatible   chrome
-// @compatible   firefox
-// @compatible   edge
-// @compatible   opera
-// @compatible   brave
-// @compatible   vivaldi
-// @compatible   waterfox
-// @compatible   librewolf
-// @compatible   ghost
-// @compatible   qq
-// @compatible   whale
-// @compatible   kiwi
-// @grant        none
+// @grant  none
 // ==/UserScript==
 
 const TARGET_URL = "https://www.youtube.com/feed/subscriptions"; // Could redirect to anything, but honestly I'd just redirect it to Subscriptions
